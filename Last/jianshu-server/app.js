bf8e90bb8a26c56bab393574a915d7149acdd734
Zhou <zhoushengzhen2019@gmail.com>
2022-06-17 09:05:19 +0800

    week 1

--- Last/jianshu-server/app.js
+++ Last/jianshu-server/app.js
@@ -1,42 +1,48 @@
-const Koa = require('koa')
+const Koa = require("koa")
 const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const MongoConnect = require('./db')
-const koajwt = require('koa-jwt')
-const cors = require('koa2-cors')
+const views = require("koa-views")
+const json = require("koa-json")
+const onerror = require("koa-onerror")
+const bodyparser = require("koa-bodyparser")
+const logger = require("koa-logger")
+const MongoConnect = require("./db")
+const koajwt = require("koa-jwt")
+const cors = require("koa2-cors")
 
-const index = require('./routes/index')
-const users = require('./routes/users')
-const upload = require('./routes/upload')
-const article = require('./routes/article')
-const comment = require('./routes/comment')
-const fans = require('./routes/fans')
+const index = require("./routes/index")
+const users = require("./routes/users")
+const upload = require("./routes/upload")
+const article = require("./routes/article")
+const comment = require("./routes/comment")
+const fans = require("./routes/fans")
 
 // error handler
 onerror(app)
 MongoConnect()
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"]
+  })
+)
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require("koa-static")(__dirname + "/public"))
 app.use(cors())
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "ejs"
+  })
+)
 
-app.use(koajwt({
-  secret: 'jianshu-server-jwt'
-}).unless({
-  path: [/^\/users\/login/,/^\/users\/reg/]
-}))
+app.use(
+  koajwt({
+    secret: "jianshu-server-jwt"
+  }).unless({
+    path: [/^\/users\/login/, /^\/users\/reg/]
+  })
+)
 
 // logger
 app.use(async (ctx, next) => {
@@ -55,8 +61,8 @@ app.use(comment.routes(), comment.allowedMethods())
 app.use(fans.routes(), fans.allowedMethods())
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx)
+})
 
 module.exports = app
